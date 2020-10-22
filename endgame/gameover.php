946252da24897a191d71cda409c99066623644bb
Alejandro Nieto Luque <aleexnl@MacBook-Pro-de-Alejandro.local>
2020-10-22 16:39:23 +0200

    Movido css a archivos correspondientes

--- endgame/gameover.php
+++ endgame/gameover.php
@@ -5,67 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>You Win</title>
-    <style>
-        @font-face {
-            font-family: AmongUsFont;
-            src: url("./VCR_OSD_MONO.ttf");
-        }
-
-        * {
-            color: white;
-            font-family: AmongUsFont;
-        }
-
-        body#victory {
-            background-color: black;
-            display: flex;
-            flex-flow: row wrap;
-        }
-
-        p#title {
-            font-size: 120px;
-            font-weight: bold;
-            color: #e44024;
-            text-align: center;
-            width: 100%
-        }
-
-        div.box {
-            width: 75%;
-            background-color: #821f0b;
-            box-shadow: 0 0 65px 75px #821f0b;
-            margin: auto;
-            border: 0px #0000;
-            margin-top: 35px;
-            border-radius: 29px;
-            display: flex;
-            flex-flow: row wrap;
-        }
-
-        p#text {
-            font-size: 33px;
-            color: #000;
-            text-align: center;
-            width: 100%;
-            margin: 5px 0;
-            font-weight: bold;
-        }
-
-        input[type="submit"] {
-            width: 120px;
-            height: 33px;
-            color: #000;
-            font-size: larger;
-        }
-
-        form#form-home {
-            margin: 25px 25px auto auto;
-        }
-
-        form#form-retry {
-            margin: 25px auto auto 25px;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/gameover.css">
 </head>
 
 <body id="victory">
--- endgame/victory.php
+++ endgame/victory.php
@@ -5,67 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>You Win</title>
-    <style>
-        @font-face {
-            font-family: AmongUsFont;
-            src: url("./VCR_OSD_MONO.ttf");
-        }
-
-        * {
-            color: white;
-            font-family: AmongUsFont;
-        }
-
-        body#victory {
-            background-color: black;
-            display: flex;
-            flex-flow: row wrap;
-        }
-
-        p#title {
-            font-size: 120px;
-            font-weight: bold;
-            color: #0887fa;
-            text-align: center;
-            width: 100%
-        }
-
-        div.box {
-            width: 75%;
-            background-color: #0ff;
-            box-shadow: 0 0 65px 75px #0ff;
-            margin: auto;
-            border: 0px #0000;
-            margin-top: 35px;
-            border-radius: 29px;
-            display: flex;
-            flex-flow: row wrap;
-        }
-
-        p#text {
-            font-size: 33px;
-            color: #000;
-            text-align: center;
-            width: 100%;
-            margin: 5px 0;
-            font-weight: bold;
-        }
-
-        input[type="submit"] {
-            width: 120px;
-            height: 33px;
-            color: #000;
-            font-size: larger;
-        }
-
-        form#form-home {
-            margin: 25px 25px auto auto;
-        }
-
-        form#form-next {
-            margin: 25px auto auto 25px;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/victory.css">
 </head>
 
 <body id="victory">
