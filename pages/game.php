484fd814e65c0af94251417b72e67d6d4b50bf65
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-10-25 11:13:02 +0100

    Delketed unused code

--- pages/game.php
+++ pages/game.php
@@ -18,15 +18,16 @@
     }
     ?>
 </head>
+
 <body>
     <header>
         <h2>Simon says</h2>
         <div id="home"><a href="../">Home</a></div>
         <h3 id="uname">
             <?php
-                session_start();
-                $_SESSION["user"] = isset($_GET["uname"]) ? $_GET["uname"] : '';
-                echo isset($_SESSION["user"]) ? $_SESSION['user'] : '';
+            session_start();
+            $_SESSION["user"] = isset($_GET["uname"]) ? $_GET["uname"] : '';
+            echo isset($_SESSION["user"]) ? $_SESSION['user'] : '';
             ?>
         </h3>
     </header>
@@ -46,52 +47,15 @@
                     for ($rowCounter = 0; $rowCounter < 5; $rowCounter++) {
                         echo "<tr>";
                         for ($columnounter = 0; $columnounter < 5; $columnounter++) {
-                            /*if (in_array($randomCounter++, $randomNumbers)) {
+                            if (in_array($randomCounter++, $randomNumbers)) {
                                 echo "<td><button type='submit' class='option selected'></button></td>";
-                            } else {*/
+                            } else {
                                 echo "<td><button type='submit' class='option' name='btn-option'></button></td>";
-                            //}
+                            }
                         }
                         echo "</tr>";
                     }
                     ?>
-                    <!--
-                    <tr>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                    </tr>
-                    <tr>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                    </tr>
-                    <tr>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                    </tr>
-                    <tr>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                    </tr>
-                    <tr>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                        <td><button type="submit" class="option"></button></td>
-                    </tr>
--->
                 </tbody>
                 <tfoot>
                     <tr>
