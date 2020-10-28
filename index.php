086a73a0aa675c044775291190d95b1b6a8f3dea
jcsalinas20 <jcsalinasn20@gmail.com>
2020-10-28 18:49:25 +0100

    autocomplete username in Home

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
         <title>Simon-says</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" href="css/index.css">
+        <?php session_start(); ?>
     </head>
     <body>
         <header>
@@ -16,7 +17,7 @@
         <h2 class="subtitle">Welcome to our site! Here you can enjoy the Simon says game online! Insert you user name and press the start game button to begin!</h2>
         <form id="form" action="pages/game.php" method="GET">
             <label for="uname"><b>USERNAME:</b></label>
-            <input type="text" placeholder="Neo" name="uname" required><br>
+            <input type="text" placeholder="Neo" name="uname" value="<?= isset($_SESSION['user']) ? $_SESSION['user'] : ''; ?>" required><br>
             <input type="submit" value="Start game" href="index.html"></input>
         </form>
     </body>
