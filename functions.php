d47d1a18ce3a329c00826aa2238d6d456f1ccd70
Alejandro Nieto Luque <aleexnl@MacBook-Pro-de-Alejandro.local>
2020-10-29 15:24:21 +0100

    On username change, reset to level 0

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function get_level($level_number)
 {
-    $levels = file("levels.cfg");
+    $levels = file(__DIR__ . "/levels.cfg"); // __DIR__ is used to get the absolute path of the file.
     $level = explode(";", $levels[$level_number]);
     array_push($level, $level_number);
     return $level;
