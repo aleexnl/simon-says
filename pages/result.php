288598f1ca875cc2a46ddf72392e2236dde8e094
jcsalinas20 <jcsalinasn20@gmail.com>
2020-10-29 15:55:29 +0100

    save punctuation and username of user

--- pages/result.php
+++ pages/result.php
@@ -3,10 +3,14 @@
 session_start();
 
 if (isset($_GET['result'])) {
+    $_SESSION['punctuation'] = 200;
+
     if ($_GET['result'] == "win") {
+        if($_SESSION['actual_level'][5] == 9) writeInRankingFile(getUserDetails());
         $_SESSION['endgame'] = "win";
         header("location:victory.php");
-    }else if ($_GET['result'] == "lose") {
+    } else if ($_GET['result'] == "lose") {
+        writeInRankingFile(getUserDetails());
         $_SESSION['endgame'] = "lose";
         header("location:gameover.php");
     }
