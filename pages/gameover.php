fa9fe22b298b504e5a6cedd5bb7e555250f258c9
Alejandro Nieto Luque <aleexnl@MacBook-Pro-de-Alejandro.local>
2020-11-02 16:23:52 +0100

    Deleted line where code was duplicated

--- pages/gameover.php
+++ pages/gameover.php
@@ -27,7 +27,6 @@
     <p id="title">Game Over</p>
     <div class="box">
         <p id="text">You didn't pass the <?= $_SESSION["actual_level"][0] ?> level</p>
-        <p id="text"><small>Level code: <code><?= $_SESSION["actual_level"][4] ?></code></small></p>
         <form action="../" method="post" id="form-home">
             <input type="submit" value="Home" accesskey="H" />
         </form>
