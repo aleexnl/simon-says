fd322229c0319c7ea0c8f885f2cf6b3a574386c3
JesusSePe <57097178+JesusSePe@users.noreply.github.com>
2020-11-04 18:31:13 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,33 +1,67 @@
-let buttons = document.getElementsByClassName("option"); // Get all buttons
-let resolve_button = document.getElementById("btn-resolve");
+let buttons = document.getElementsByClassName("option"); // Get all buttons.
+let resolve_button = document.getElementById("btn-resolve"); // get resolve button.
 let start_button = document.getElementById("btn-start");
-let time = 0;
+let correctButtons = document.getElementById("correct-squares"); // get the number of correct squares.
+let showTime = document.getElementById("show-time");
+let time = 0; // Time variable
 let intervalTimer = "";
 
-start_button.onclick = function () {
-    let solutions = document.getElementsByClassName("solution");
-    for (const solution of solutions) {
-        solution.classList.add("selected");
+function redirectPage(endgame, time) {
+    // Redirect to the corresponding result page.
+    window.location.href = `./result.php?result=${endgame}&time=${time}`;
+}
+
+function timer() {
+    // Timer for every second.
+    setInterval(() => {
+        time++;
+    }, 1000);
+}
+
+function enableButtons(buttons) {
+    // Enable all buttons from the game board.
+    for (const button of buttons) {
+        button.removeAttribute("disabled");
     }
-    resolve_button.setAttribute("disabled", true);
+}
+
+function showSolutions(buttons) {
+    // Show all the solutions.
+    for (const button of buttons) {
+        button.classList.add("selected");
+    }
+}
+
+function hideSolutions(buttons) {
+    // Hide all solutions.
+    for (const button of buttons) {
+        button.classList.remove("selected");
+    }
+}
+
+start_button.onclick = function () {
+    let solutions = document.getElementsByClassName("solution"); // Get all solutions
+    showSolutions(solutions); // Show solutions
+    resolve_button.setAttribute("disabled", true); // Disable resolve button
     setTimeout(() => {
-        timer();
-        enableButtons(buttons);
-        for (const solution of solutions) {
-            solution.classList.remove("selected");
-        }
-        resolve_button.removeAttribute("disabled");
-        start_button.setAttribute("disabled", true);
-    }, 4000);
+        // After 4 seconds
+        timer(); // enable timer
+        enableButtons(buttons); // enable buttons
+        hideSolutions(solutions); // hide solutions
+        resolve_button.removeAttribute("disabled"); // Enable resolve button
+        start_button.setAttribute("disabled", true); // Disable start button
+    }, showTime.innerText * 1000);
 };
 
 resolve_button.onclick = function () {
     let solution = document.getElementsByClassName("selected solution");
     let selectedButtons = document.getElementsByClassName("selected");
-    clearInterval(intervalTimer)
-
-    if (solution.length == 7 && selectedButtons.length == 7) {
-        redirectPage("win",time);
+    clearInterval(intervalTimer);
+    if (
+        solution.length == correctButtons.innerText &&
+        selectedButtons.length == correctButtons.innerText
+    ) {
+        redirectPage("win", time);
     } else {
         redirectPage("lose", time);
     }
@@ -42,20 +76,4 @@ for (const button of buttons) {
             button.classList.add("selected"); // Add the option that is selected
         }
     };
-}
-
-function redirectPage(endgame, time) {
-    window.location.href = "./result.php?result=" + endgame + "&userTime=" + time;
-}
-function timer() {
-    intervalTimer = setInterval(() => {
-        time++;
-        console.log(time);
-    }, 1000);
-}
-
-function enableButtons(buttons) {
-    for (const button of buttons) {
-        button.removeAttribute("disabled");
-    }
 }
